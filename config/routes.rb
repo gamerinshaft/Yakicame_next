eeb1f4dfb29a1491fbfd819383d76d3173fef01d
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-10-28 00:05:55 +0900

    fa,bsを引っ張ってくる設定＋sassを使う準備

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,13 @@ Yaki::Application.routes.draw do
 
   resources :tweets
 
+  unauthenticated do
+    as :user do
+      root to: 'homes#index'
+    end
+  end
 
-  root to: "tweets#index"
+  root to: 'tweets#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
