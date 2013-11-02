5c195c96baf4f17802f237ad100a584e58f0a683
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-11-02 12:08:18 +0900

    親子関係を書いた

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,5 @@
 class Tweet < ActiveRecord::Base
   attr_accessible :content, :user_id, :image
+  belongs_to :user
   mount_uploader :image, ImageUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   attr_accessor :login
   attr_accessible :login
   
-  
+  has_many :tweets
 
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
