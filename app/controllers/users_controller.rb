e2e7be6c220bfce9f05eee0fc538a02631be35d3
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-11-09 19:53:03 +0900

    users show を編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,8 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   	
+  	# @followers = User.find(@user.inverse_follows.pluck(user_id))
+  	# @follwer = Follow.find(params[:id])
+  	# @follower = User.find(@user.inverse_follows.user_id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,8 @@ class User < ActiveRecord::Base
   has_many :follows
   has_many :following_users, through: :follows, source: :user
   has_many :inverse_follows, class_name: Follow, foreign_key: :followed_id
-  has_many :followed_users, through: :inverse_follows
+  has_many :followed_users, through: :inverse_follows, source: :user
+
   has_many :tweets, dependent: :destroy
   has_many :favorites
   mount_uploader :image, ImageUploader
