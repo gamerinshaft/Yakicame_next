8a7bfdb54e4501d13457584b29e26238f6e6a587
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-11-11 00:04:31 +0900

    user indexにkaminari実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
 
   def index
-  	@users = User.all
+  	@users = User.order("id").page(params[:page]).per(8)
 
-  	
+
   end
 
   def show
   	@user = User.find(params[:id])
     @users = User.all
-  	
+
   	# @followers = User.find(@user.inverse_follows.pluck(user_id))
   	# @follwer = Follow.find(params[:id])
   	# @follower = User.find(@user.inverse_follows.user_id)
